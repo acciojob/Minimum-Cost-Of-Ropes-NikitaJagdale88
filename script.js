10f83f9048b50e63883bb38e2a7536722adc7176
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-21 13:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,115 @@
-function calculateMinCost() {
-  const str = document.querySelector('#rope-lengths').value;
-  const strArr = str.split(',');
-  const arr = strArr.map(e => Number.parseInt(e));
+// Function to calculate minimum cost of ropes
+function calculateMinimumCostRopes() {
+  // Get the input values from the text field
+  var input = document.getElementById("ropes-input").value;
 
-  let n = arr.length;
+  // Split the input string into an array of rope lengths
+  var ropes = input.split(",").map(Number);
 
-  let ans = helper(arr, n);
-  document.querySelector('#result').innerText = ans;
-}
+  // Create a priority queue (min heap)
+  var priorityQueue = new MinHeap();
 
-function helper(arr, n) {
-  // Create a priority queue
-  let pq = [];
-
-  // Adding items to the pQueue
-  for (let i = 0; i < n; i++) {
-    pq.push(arr[i]);
+  // Insert all ropes into the priority queue
+  for (var i = 0; i < ropes.length; i++) {
+    priorityQueue.insert(ropes[i]);
   }
 
-  pq.sort(function(a, b) {
-    return a - b;
-  });
+  // Variable to store the total cost
+  var totalCost = 0;
 
-  // Initialize result
-  let res = 0;
+  // Merge ropes until only one rope is left in the priority queue
+  while (priorityQueue.size() > 1) {
+    // Extract the two smallest ropes from the priority queue
+    var rope1 = priorityQueue.extractMin();
+    var rope2 = priorityQueue.extractMin();
 
-  // While size of priority queue
-  // is more than 1
-  while (pq.length > 1) {
-    // Extract shortest two ropes from pq
-    let first = pq.shift();
-    let second = pq.shift();
+    // Calculate the cost of merging the ropes
+    var cost = rope1 + rope2;
 
-    // Connect the ropes: update result
-    // and insert the new rope to pq
-    res += first + second;
-    pq.push(first + second);
+    // Add the cost to the total cost
+    totalCost += cost;
 
-    pq.sort(function(a, b) {
-      return a - b;
-    });
+    // Insert the merged rope back into the priority queue
+    priorityQueue.insert(cost);
   }
-  return res;
+
+  // Output the minimum cost of connecting the ropes
+  document.getElementById("result").innerHTML = totalCost;
 }
- 
+
+// MinHeap class implementation
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp(this.heap.length - 1);
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+
+    var minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown(0);
+    return minValue;
+  }
+
+  heapifyUp(index) {
+    var parentIndex = Math.floor((index - 1) / 2);
+
+    if (parentIndex >= 0 && this.heap[parentIndex] > this.heap[index]) {
+      this.swap(parentIndex, index);
+      this.heapifyUp(parentIndex);
+    }
+  }
+
+  heapifyDown(index) {
+    var leftChildIndex = 2 * index + 1;
+    var rightChildIndex = 2 * index + 2;
+    var smallestIndex = index;
+
+    if (
+      leftChildIndex < this.heap.length &&
+      this.heap[leftChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = leftChildIndex;
+    }
+
+    if (
+      rightChildIndex < this.heap.length &&
+      this.heap[rightChildIndex] < this.heap[smallestIndex]
+    ) {
+      smallestIndex = rightChildIndex;
+    }
+
+    if (smallestIndex !== index) {
+      this.swap(smallestIndex, index);
+      this.heapifyDown(smallestIndex);
+    }
+  }
+
+  swap(index1, index2) {
+    var temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+}
+
+// Call the function to calculate minimum cost when the form is submitted
+document.getElementById("ropes-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  calculateMinimumCostRopes();
+});
